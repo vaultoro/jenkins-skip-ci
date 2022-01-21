7a2151e53a2e2e66d88b3377733e15f2a4e78f38
berat <89453188+beratvaultoro@users.noreply.github.com>
2022-01-21 16:21:23 +0100

    added functions in the vars folder

---
+++ vars/skip-ci.groovy
@@ -0,0 +1,27 @@
+def get_skip_ci_options() {
+  def skipCi = false
+  def skipTest = false
+  echo "getting latest commit message"
+
+  def latestCommitMessage = sh (
+  script: 'git log -1 --pretty=%B ${GIT_COMMIT}', returnStdout: true
+  ).trim()
+
+  echo "Latest commit message is: ${latestCommitMessage}"
+
+  if (latestCommitMessage.contains('skip ci') || latestCommitMessage.contains('ci skip')) {
+	skipCi = true
+  }
+  echo "Skip ci variable is ${skipCi}"
+
+  if (latestCommitMessage.contains('skip test') || latestCommitMessage.contains('test skip')) {
+	skipTest = true
+  }
+  echo "Skip test variable is ${skipTest}"
+  return [skipCi, skipTest]
+}
+
+def say_hello() {
+    def name = "BERATBERATBERAT"
+    return name
+}
